b0c4cb7136ed231ff1c7ca8a22fbc0f4e968451c
Andrew Jones <me@andrewhjon.es>
2011-06-06 22:56:40 +0100

    Can click the new dice that are created to throw. New dice are added before the ?

--- dice.js
+++ dice.js
@@ -1,18 +1,28 @@
-var throw_dice = function() {
-    var generate = function() {
-        return 1 + (Math.floor(Math.random() * 6));
-    };
+var generate = function() {
+    return 1 + (Math.floor(Math.random() * 6));
+};
 
+var throw_dice = function() {
     var els = document.getElementsByClassName("number");
     for (var i = 0; i < els.length; i++) {
         els[i].innerHTML = generate().toString();
     }
 };
 
+var new_dice = function () {
+    var body = document.getElementsByTagName("body")[0];
+    var ghost = document.getElementsByClassName("ghost")[0];
+
+    var dice = document.createElement("span");
+
+    dice.setAttribute("class", "number dice");
+    dice.innerHTML = generate().toString();
+    dice.onclick = throw_dice;
+
+    body.insertBefore(dice, ghost);
+};
+
 window.onload = function () {
-    var els = document.getElementsByClassName("number");
-    for (var i = 0; i < els.length; i++) {
-        els[i].onclick = throw_dice;
-    }
-    throw_dice();
+    var ghost = document.getElementsByClassName("ghost")[0];
+    ghost.onclick = new_dice;
 };
