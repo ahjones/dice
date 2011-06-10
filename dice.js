6bfec129a17bb244f9df5ccba6d4c2f737dadf68
Andrew Jones <me@andrewhjon.es>
2011-06-10 17:41:48 +0100

    use template to create dice

--- dice.js
+++ dice.js
@@ -9,17 +9,35 @@ var throw_dice = function() {
     }
 };
 
+var get_dice_template = function () {
+    var request = new XMLHttpRequest();
+    request.open("GET", "dice_template.html", false);
+    request.send(null);
+    return request.responseText;
+}
+
 var new_dice = function () {
+    var template = get_dice_template();
+    var number = generate().toString();
+
+    var html = template.replace("{{num}}", number);
+    var dice = load_html(html);
+    
     var container = document.getElementsByClassName("container")[0];
-    var ghost = document.getElementsByClassName("ghost")[0];
-
-    var dice = document.createElement("div");
-
-    dice.setAttribute("class", "number dice");
-    dice.innerHTML = generate().toString();
-    dice.onclick = throw_dice;
-
+    var ghost = document.getElementsByClassName("ghost")[0].parentNode;
     container.insertBefore(dice, ghost);
+}
+
+var load_html = function (html_string) {
+    var elt = document.createElement("div");
+    var frag = document.createDocumentFragment();
+    elt.innerHTML = html_string;
+
+    while (elt.firstChild) {
+        frag.appendChild(elt.firstChild);
+    }
+
+    return frag;
 };
 
 window.onload = function () {
