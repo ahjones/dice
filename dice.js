48c1efc7d362575405d22b2cc306800d6f080248
Andrew Jones <me@andrewhjon.es>
2011-06-10 19:18:30 +0100

    give dice unique ids

--- dice.js
+++ dice.js
@@ -16,16 +16,24 @@ var get_dice_template = function () {
     return request.responseText;
 }
 
+var next_dice_id = 0;
 var new_dice = function () {
     var template = get_dice_template();
     var number = generate().toString();
+    var dice_id = "dice_" + next_dice_id;
+    next_dice_id++;
 
-    var html = template.replace("{{num}}", number);
+    var html = template.replace("{{num}}", number)
+                       .replace("{{id}}", dice_id);
+
     var dice = load_html(html);
-    
+
     var container = document.getElementsByClassName("container")[0];
     var ghost = document.getElementsByClassName("ghost")[0].parentNode;
     container.insertBefore(dice, ghost);
+    document.getElementById(dice_id)
+            .getElementsByClassName("number")[0]
+            .onclick = throw_dice;
 }
 
 var load_html = function (html_string) {
